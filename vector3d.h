82a897768fd1f9334333c998336ddd3be18ded7e
Thomas Georgiou <thomas@Poseidon.(none)>
2009-03-29 12:47:49 -0400

    Explicitly inline methods

--- vector3d.h
+++ vector3d.h
@@ -4,16 +4,16 @@ class Vector3D {
 	public:
 		Vector3D(double x0 = 0.0, double y0 = 0.0, double z0 = 0.0);
 		double x, y, z;
-		double mag() const;
-		double mag_sq() const;
+		inline double mag() const;
+		inline double mag_sq() const;
 		void zero();
-		Vector3D operator+(const Vector3D &right) const;
-		Vector3D operator-(const Vector3D &right) const;
-		Vector3D& operator+=(const Vector3D &right);
-		Vector3D operator/(const double &right) const;
-		Vector3D& operator/=(const double &right);
-		Vector3D operator*(const double &right) const;
-		Vector3D& operator*=(const double &right);
+		inline Vector3D operator+(const Vector3D &right) const;
+		inline Vector3D operator-(const Vector3D &right) const;
+		inline Vector3D& operator+=(const Vector3D &right);
+		inline Vector3D operator/(const double &right) const;
+		inline Vector3D& operator/=(const double &right);
+		inline Vector3D operator*(const double &right) const;
+		inline Vector3D& operator*=(const double &right);
 		double operator*(const Vector3D &right) const;
 		double& operator[](const int index);
 };
